b4ac4ada802273a3b29cc4112ee607ea9982941d
Nithya-collab <snithiyadharshini7@gmail.com>
2024-08-19 12:16:04 +0530

    change dashboard events routes

--- app/dashboard/events/[eventId]/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MyComponent = ({params}) =>{
-    return (
-      <div> Edit the Events for {params.eventId} </div>
-    );
-}
-
-export default MyComponent;
---
+++ app/dashboard/events/[eventId]/ticketCategories/[ticketCategoryId]/edit/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditTicketCategory({params}) {
+  return (
+    <div> EditTicketCategory.....</div>
+  )
+}
+
+export default EditTicketCategory
